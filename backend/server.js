43b1cc4b4bdd67cec62e97b8c431beab6f7df3a3
Armaan_Seydi <armaanseydi@gmail.com>
2024-04-13 14:29:48 +0330

    setup: auth routes

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,7 @@
 import express from "express";
 import { config } from "dotenv";
+import authRoutes from "./routes/auth.routes.js";
+
 const app = express();
 
 config();
@@ -9,5 +11,17 @@ app.get("/", (req, res) => {
   //root route http://localhost:5000/
   res.send("Hello world!");
 });
+
+app.use("/api/auth", authRoutes);
+
+// app.get("/api/get/signup", (req, res) => {
+//   console.log("signup route");
+// });
+// app.get("/api/get/logout", (req, res) => {
+//   console.log("logout route");
+// });
+// app.get("/api/get/logout", (req, res) => {
+//   console.log("logout route");
+// });
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
