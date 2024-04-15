ee4a51559f3a2e0681e07d0c8ba444bb815a3e8e
Armaan_Seydi <armaanseydi@gmail.com>
2024-04-15 15:41:16 +0330

    implement: send message controller

--- backend/models/conversation.model.js
+++ backend/models/conversation.model.js
@@ -2,12 +2,20 @@ import mongoose from "mongoose";
 
 const conversationSchema = new mongoose.Schema(
   {
-    participants: [{ type: mongoose.Schema.Types.ObjectId, ref: "User" }],
-    message: [
-      { type: mongoose.Schema.Types.ObjectId, ref: "Message", default: [] },
+    participants: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User",
+      },
+    ],
+    messages: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Message",
+        default: [],
+      },
     ],
   },
-  // createdAt, updatedAt
   { timestamps: true }
 );
 
