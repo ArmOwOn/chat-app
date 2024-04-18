bb546fedfdb43a01de53bd210f2b792e31210839
Armaan_Seydi <armaanseydi@gmail.com>
2024-04-18 16:56:23 +0330

    socket.io implemented

--- backend/server.js
+++ backend/server.js
@@ -1,25 +1,36 @@
 /* eslint-disable no-undef */
+import path from "path";
 import express from "express";
-import { config } from "dotenv";
+import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
+
 import authRoutes from "./routes/auth.routes.js";
 import messageRoutes from "./routes/message.routes.js";
 import userRoutes from "./routes/user.routes.js";
-import connectToMongoDB from "./db/connectToMongoDB.js";
 
-const app = express();
+import connectToMongoDB from "./db/connectToMongoDB.js";
+import { app, server } from "./socket/socket.js";
+
 const PORT = process.env.PORT || 5000;
 
-config();
+const __dirname = path.resolve();
 
-app.use(express.json());
+dotenv.config();
+
+app.use(express.json()); // to parse the incoming requests with JSON payloads (from req.body)
 app.use(cookieParser());
 
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 app.use("/api/users", userRoutes);
 
-app.listen(PORT, () => {
+app.use(express.static(path.join(__dirname, "/frontend/dist")));
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
+});
+
+server.listen(PORT, () => {
   connectToMongoDB().then(() =>
     console.log(`Server is running on: \x1b[34mhttp://localhost:${PORT}\x1b[0m`)
   );
